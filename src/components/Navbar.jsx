f10ffb114bf5e4a9a7be9fef9dc858323e47439a
Bhuwan-Upadhyay <bhuwanb898@gmail.com>
2024-09-28 17:01:29 +0530

    change glitch of bg img

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import Theme from "./Theme";
 
-export default function Navbar() {
+export default function Navbar({showData}) {
     return (
         <nav className="navbar flex justify-between w-auto py-2">
             <div className="cursor-pointer font-bold text-lg mx-5">TaskMater</div>
             <ul className='flex gap-5 mx-5'>
                 <li className="cursor-pointer hover:font-bold transition-all">Home</li>
-                <li className="cursor-pointer hover:font-bold transition-all">Your Tasks</li>
+                <li className="cursor-pointer hover:font-bold transition-all" onClick={showData}>Your Tasks</li>
                 <li className="cursor-pointer hover:font-bold transition-all"><Theme /></li>
             </ul>
         </nav>
